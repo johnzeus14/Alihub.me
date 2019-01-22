a2a934f272f95355019f86ae4a118c07c815d830
john benjamin <johnzeus14@gmail.com>
2019-01-22 13:14:56 +0100

    updated test for the profile but still dont understand somethings

--- src/alihub/userprofile/tests.py
+++ src/alihub/userprofile/tests.py
@@ -1,105 +1,138 @@
-# from  rest_framework.test import APITestCase
-# from rest_framework.reverse import reverse
-# from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
 
 
-# from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.settings import api_settings
 
-# payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-# encode_handler  = api_settings.JWT_ENCODE_HANDLER
-
-
-
-# from django.contrib.auth import get_user_model
-# from django.contrib.auth import authenticate
-
-
-# from .models import Profile
-
-
-# User = get_user_model()
+payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+encode_handler  = api_settings.JWT_ENCODE_HANDLER
 
 
 
-# class ProfileAPITestCase(APITestCase):
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 
 
-# 	def setUp(self):
 
-# 		user_obj  = User( email = 'zeus@gmail.com', full_name = 'john benjamin')
-# 		user_obj.set_password ('oldskool123')
-# 		user_obj.save()
+
+from .models import Profile
+
+
+
+User = get_user_model()
+
+
+class ProfileAPITestCase(APITestCase):
+
+
+	def setUp(self):
+
+		user_obj  = User( email = 'zeus@gmail.com', full_name = 'john benjamin')
+		user_obj.set_password ('oldskool123')
+		user_obj.save()
+
+	
+		profile = Profile.objects.create(
+				user  			= user_obj,
+				username		= 'ALihub',
+				avatar			= None,
+				bio				= 'The official page of Alihub',
+				category 		= 'Telecomunication',
+				country 		=  'Nigeria',
+				phone 			=  '0809740825',
+				website 		= 'alihub.com',
+
+			)
 
 	
 
-# 		profile = Profile.objects.create(
-# 			user  			= user_obj,
-# 			username		= 'zeus',
-# 			avatar			= None,
-# 			bio				= 'The official page of Alihub',
-# 			category 		= 'Telecomunication',
-# 			country 		=  'Nigeria',
-# 			phone 			=  '0809740825',
-# 			website 		= 'alihub.com',
-			
 
-# 			)
+
+	def test_single_user(self):
+		user_obj = User.objects.count()
+		self.assertEqual(user_obj, 1)
+
+
+	def test_single_profile(self):
+		profile  = Profile.objects.count()
+		self.assertEqual(profile, 1)
 
 
 
-# 	def test_single_user(self):
-# 		user_obj = User.objects.count()
-# 		self.assertEqual(user_obj, 1)
-# 		print(user_obj)
 
+	def test_post_profile_with_user(self):
+		user_obj  = User.objects.first()
+		payload   = payload_handler(user_obj)
+		token_rsp = encode_handler(payload)
+		print(token_rsp)
+		print(user_obj)
 
-# 	def test_single_profile(self):
-# 		profile  = Profile.objects.count()
-# 		self.assertEqual(profile, 1)
-
-# 	def test_create_profile(self):
-# 		user_obj    =  User.objects.first()
-# 		payload 	= payload_handler(user_obj)
-# 		token_rsp	= encode_handler(payload)
-# 		self.client.credentials(HTTP_AUTHORIZATION = 'JWT'+ token_rsp)
-		
-# 		data       = {
-			
-# 			'username' 	:'zeus',
-# 			'avatar'   	:None,
-# 			'bio'	   	:'my first status',
-# 			'category'	:'Religion',
-# 			'country'	:'NIG',
-
-# 		}
-# 		url  		= reverse('profile:profile-create')
-# 		response	 = self.client.post(url, data, format = 'json')
-# 		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED )
-
-# 	def test_update_profile(self):
-# 		obj 			= Profile.objects.first()
-		
-		
-# 		user_obj		= User.objects.first()
-# 		payload 		= payload_handler(user_obj)
-# 		token_rsp		= encode_handler(payload)
-
-# 		self.client.credentials(HTTP_AUTHORIZATION = 'JWT'+ token_rsp)
-# 		url 			= obj.get_api_url()
-# 		data			= {
-
-# 				'username'	:	'alihub',
-# 				'avatar'	:	None,
-# 				'bio'		:	'hello world',
-# 				'category'	: 	'Religion',
-# 				'country'	:	'NIG',
-
-# 		}
-	
-		
-	
-# 		response 		 = self.client.put(url, data, format = 'json')
-# 		print(response.data)
-# 		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.client.credentials(HTTP_AUTHORIZATION = 'JWT' + token_rsp)
 
 	
+		
+		data			 = {
+
+				'username'		:'ALihub',
+				'avatar'		: None,
+				'bio'			: 'The official page of Alihub',
+				'category' 		:'Telecomunication',
+				'country' 		:  'Nigeria',
+				'phone' 		: '0809740825',
+				'website' 		: 'alihub.com',
+		}
+		url 			 = reverse('profile:profile-create')
+		print(data)
+		response 		 = self.client.post(url, data, format = 'json')
+		self.assertEqual = (response.status_code, status.HTTP_200_OK)
+
+
+	def test_put_review_with_user(self):
+		user_obj  = User.objects.first()
+		payload   = payload_handler(user_obj)
+		token_rsp = encode_handler(payload)
+		print(token_rsp)
+		print(user_obj)
+
+		self.client.credentials(HTTP_AUTHORIZATION = 'JWT' + token_rsp)
+		
+		
+		data			 = {
+
+				'username'		:'zenithbank',
+				'avatar'		: None,
+				'bio'			: 'The official page of Alihub',
+				'category' 		:'Telecomunication',
+				'country' 		:  'Nigeria',
+				'phone' 		: '0809740825',
+				'website' 		: 'alihub.com',
+		}
+		print(data)
+		url 			 = reverse('profile:profile-detail')
+		print(url)
+		response 		 = self.client.post(url, data, format = 'json')
+		self.assertEqual = (response.status_code, status.HTTP_200_OK)
+
+
+	# def test_delete_review_with_user(self):
+	# 	user_obj  = User.objects.first()
+	# 	payload   = payload_handler(user_obj)
+	# 	token_rsp = encode_handler(payload)
+		
+		
+
+	# 	self.client.credentials(HTTP_AUTHORIZATION = 'JWT' + token_rsp)
+		
+	# 	data			 = {}
+	# 	print( str(data) + 'the deleted data')
+	# 	review 			= Review.objects.first()
+	# 	url 			= review.get_api_url()
+
+	
+	# 	response 		 = self.client.delete(url, data, format = 'json')
+	# 	self.assertEqual = (response.status_code, status.HTTP_200_OK)
+
+
+
+	# # 	
